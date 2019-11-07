c798ad3a8b067bda8952f6463c2d76c804d9ca32
Errachete <raherrer@ucm.es>
2019-11-07 14:20:01 +0100

    Empezado comportamiento de bases

---
+++ Bot1/scripts/BaseBehavior.java
@@ -0,0 +1,66 @@
+package scripts;
+
+import ai.abstraction.AbstractAction;
+import ai.abstraction.AbstractionLayerAI;
+import ai.abstraction.Idle;
+import ai.abstraction.Train;
+import rts.PhysicalGameState;
+import rts.Player;
+import rts.units.Unit;
+import rts.units.UnitType;
+import rts.units.UnitTypeTable;
+
+enum BaseBehType{ONEWORKER};
+
+public class BaseBehavior {
+	
+	private BaseBehType baseBehType;
+	
+	private UnitTypeTable utt;
+	private UnitType workerType;
+	private UnitType baseType;
+	private UnitType barracksType;
+	private UnitType lightType;
+	private UnitType heavyType;
+	private UnitType rangedType;
+	
+	
+	public BaseBehavior(UnitTypeTable a_utt, BaseBehType a_baseBehType) {
+		reset(utt);
+		baseBehType = a_baseBehType;
+	}
+	
+	public void reset(UnitTypeTable a_utt)  
+    {
+        utt = a_utt;
+        workerType = utt.getUnitType("Worker");
+        baseType = utt.getUnitType("Base");
+        barracksType = utt.getUnitType("Barracks");
+        lightType = utt.getUnitType("Light");
+        heavyType = utt.getUnitType("Heavy");
+        rangedType = utt.getUnitType("Ranged");
+    }   
+	
+	public AbstractAction oneWorkerBeh(Unit u, Player p, PhysicalGameState pgs) {
+		int nworkers = 0;
+        for (Unit u2 : pgs.getUnits()) {
+            if (u2.getType() == workerType
+                    && u2.getPlayer() == p.getID()) {
+                nworkers++;
+            }
+        }
+        if (nworkers < 1 && p.getResources() >= workerType.cost) {
+            return new Train(u, workerType);
+        }
+        return null;
+	}
+	
+	public BaseBehType getType() {
+		return baseBehType;
+	}
+	
+	public void setType(BaseBehType a_baseBehType) {
+		baseBehType = a_baseBehType;
+	}
+	
+}
