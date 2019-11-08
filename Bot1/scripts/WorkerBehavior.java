7744133cf7d5e7c843cec4664568a2ce616d98b7
rinicro <ruben.ruper@gmail.com>
2019-11-08 22:21:15 +0100

    worker behavior hay que acabar aggresive

--- Bot1/scripts/WorkerBehavior.java
+++ Bot1/scripts/WorkerBehavior.java
@@ -4,9 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import ai.abstraction.AbstractAction;
-import ai.abstraction.Build;
 import ai.abstraction.Harvest;
-import ai.abstraction.Train;
 import rts.PhysicalGameState;
 import rts.Player;
 import rts.units.Unit;
@@ -51,7 +49,7 @@ public class WorkerBehavior {
         rangedType = utt.getUnitType("Ranged");
     }   
 	
-	public AbstractAction harvesterWorker(GeneralScript gs, Unit u, Player p, PhysicalGameState pgs) {
+	public void harvesterWorker(GeneralScript gs, Unit u, Player p, PhysicalGameState pgs) {
 		int nbases = 0;
         int nbarracks = 0;
         int resourcesUsed = 0;
@@ -111,6 +109,70 @@ public class WorkerBehavior {
                 }
             }
         }
+		
+	}
+	
+	public void aggresiveWorker(GeneralScript gs, Unit u, Player p, PhysicalGameState pgs) {
+		int nbases = 0;
+        int nbarracks = 0;
+        int resourcesUsed = 0;
+
+        for (Unit u2 : pgs.getUnits()) {
+            if (u2.getType() == baseType
+                    && u2.getPlayer() == p.getID()) {
+                nbases++;
+            }
+            if (u2.getType() == barracksType
+                    && u2.getPlayer() == p.getID()) {
+                nbarracks++;
+            }
+        }
+
+        if (nbases == 0 && buildingBase == 0 && 
+        	p.getResources() >= baseType.cost + resourcesUsed) {
+            // build a base:
+            gs.buildIfNotAlreadyBuilding(u,baseType,u.getX(),u.getY(),reservedPositions,p,pgs);
+            resourcesUsed += baseType.cost;
+        } else if (nbarracks == 0 && buildingBarracks == 0 &&
+        	p.getResources() >= barracksType.cost + resourcesUsed) {
+            // build a barracks:
+            gs.buildIfNotAlreadyBuilding(u,barracksType,u.getX(),u.getY(),reservedPositions,p,pgs);
+            resourcesUsed += barracksType.cost;
+        } else {
+            Unit closestBase = null;
+            Unit closestResource = null;
+            int closestDistance = 0;
+            for (Unit u2 : pgs.getUnits()) {
+                if (u2.getType().isResource) {
+                    int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
+                    if (closestResource == null || d < closestDistance) {
+                        closestResource = u2;
+                        closestDistance = d;
+                    }
+                }
+            }
+            closestDistance = 0;
+            for (Unit u2 : pgs.getUnits()) {
+                if (u2.getType().isStockpile && u2.getPlayer()==p.getID()) {
+                    int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
+                    if (closestBase == null || d < closestDistance) {
+                        closestBase = u2;
+                        closestDistance = d;
+                    }
+                }
+            }
+            if (closestResource != null && closestBase != null) {
+                AbstractAction aa = gs.getAbstractAction(u);
+                if (aa instanceof Harvest) {
+                    Harvest h_aa = (Harvest)aa;
+                    if (h_aa.getTarget() != closestResource || h_aa.getBase()!=closestBase) 
+                    	gs.harvest(u, closestResource, closestBase);
+                } else {
+                    gs.harvest(u, closestResource, closestBase);
+                }
+            }
+        }
+		
 	}
 	
 	public WorkBehType getType() {
